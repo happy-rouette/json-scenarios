0d4b35c0553fcbbce672f434a8bb2126e2acb5ea
Remi <remithomas2602@gmail.com>
2022-04-15 01:22:12 +0200

    Toggle scrollview

--- Assets/_Gingerbread/Scripts/ActionDisplayer.cs
+++ Assets/_Gingerbread/Scripts/ActionDisplayer.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionDisplayer : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _directActionText;
+    [SerializeField] private Toggle _actionsToggle;
+    [SerializeField] private ScrollRect _actionScrollView;
 
     private Interactable _firstInteractable;
 
     private void Start() 
     {
         Interactable.OnMessage += (msg, type) => _directActionText.text = "<sprite index=" + ((int) type) + "> " + msg;
+        _actionScrollView.gameObject.SetActive(_actionsToggle.isOn);
+        _actionsToggle.onValueChanged.AddListener(
+            (show) => _actionScrollView.gameObject.SetActive(show)
+        );
     }
 }
