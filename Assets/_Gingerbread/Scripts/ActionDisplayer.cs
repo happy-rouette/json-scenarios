16b8c1eaee271aa1240e98dda0f1242fa9fad0c0
Remi <remithomas2602@gmail.com>
2022-04-15 14:04:53 +0200

    Action list population

--- Assets/_Gingerbread/Scripts/ActionDisplayer.cs
+++ Assets/_Gingerbread/Scripts/ActionDisplayer.cs
@@ -9,15 +9,28 @@ public class ActionDisplayer : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _directActionText;
     [SerializeField] private Toggle _actionsToggle;
     [SerializeField] private ScrollRect _actionScrollView;
-
-    private Interactable _firstInteractable;
+    [SerializeField] private RectTransform _scrollContainer;
+    [SerializeField] private TextMeshProUGUI _actionTextPrefab;
 
     private void Start() 
     {
-        Interactable.OnMessage += (msg, type) => _directActionText.text = "<sprite index=" + ((int) type) + "> " + msg;
+        Interactable.OnMessage += DisplayMessage;
+        
         _actionScrollView.gameObject.SetActive(_actionsToggle.isOn);
         _actionsToggle.onValueChanged.AddListener(
             (show) => _actionScrollView.gameObject.SetActive(show)
         );
+
+        foreach (Transform child in _scrollContainer)
+            Destroy(child.gameObject);
+    }
+
+    private void DisplayMessage(string msg, MessageType type)
+    {
+        string text = "<sprite index=" + ((int) type) + "> " + msg;
+
+        _directActionText.text = text;
+
+        Instantiate(_actionTextPrefab, _scrollContainer).text = text;
     }
 }
